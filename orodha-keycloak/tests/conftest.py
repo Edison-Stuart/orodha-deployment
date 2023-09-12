ae5a059d8f0edc5c459db532100af289e40f9aad
Edison <edisonstuart@gmail.com>
2023-09-12 17:40:42 -0400

    Moved a few items around and renamed some directories.

--- orodha-keycloak/tests/conftest.py
+++ orodha-keycloak/tests/conftest.py
@@ -3,12 +3,10 @@ This module contains two fixtrues which supply data to our KeycloakConnection mo
 lieu of using the python-keycloak package to connect to our server.
 """
 import pytest
-from unittest.mock import MagicMock
-from unittest.mock import mo
+import unittest.mock
+from fixtures import MOCKDATA
 
-
-@pytest.fixture
-class MockKeycloakOpenIDConnection(MagicMock):
+class MockKeycloakOpenIDConnection(unittest.mock.MagicMock):
 
     def __init__(self, **kwargs):
         self.argumetns = dict(kwargs)
@@ -23,26 +21,9 @@ class MockKeycloakOpenIDConnection(MagicMock):
     def delete_user(self):
         return "user deleted"
 
-
 @pytest.fixture
-def mock_create_admin_connection(MockKeycloakOpenIDConnection, **kwargs):
-
-    keycloak_connection = MockKeycloakOpenIDConnection(
-        server_url=kwargs["server_user"],
-        username=kwargs["username"],
-        password=kwargs["password"],
-        realm_name=kwargs["realm_name"],
-        client_id=kwargs["client_id"],
-        client_secret_key=kwargs["client_secret_key"],
-        verify=True
-    )
-
-    return keycloak_connection
-
-
-@pytest.fixture
-def mock_keycloak_connection(mock):
-    mock.patch(
-        "orodha_keycloak.connection.KeycloakConnection",
-        return_value=MockKeycloakConnection(**TEST_CONNECTION_ARGS)
+def mock_create_admin_connection(mocker):
+    mocker.patch(
+        "src.orodha_keycloak.connections.create_admin_connection",
+        return_value=MockKeycloakOpenIDConnection
     )
--- orodha-keycloak/tests/fixtures/keycloak.py
+++ orodha-keycloak/tests/fixtures/keycloak.py
@@ -1,2 +1,5 @@
 TEST_CONNECTION_ARGS = {"some": 0,
                         "data": 0}
+MOCK_DATA = {
+    
+}
